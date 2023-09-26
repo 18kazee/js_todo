90b73d6746103bd801be99f106d6ac2625970654
18kazee <18kazee2415@gmail.com>
2023-09-26 23:22:54 +0900

    Add: 追加ボタンを押すとテキストボックスの内容を取得

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import "./styles.css";
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const innerText = document.getElementById("add-text").value;
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+
