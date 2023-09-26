83715793412460a01883c72f2aeab0700aa0581e
18kazee <18kazee2415@gmail.com>
2023-09-27 00:04:22 +0900

    Add: 追加ボタンを押した時に削除ボタンタグを生成

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ const onClickAdd = () => {
   //button(完了)タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  console.log(completeButton);
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  console.log(deleteButton);
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
