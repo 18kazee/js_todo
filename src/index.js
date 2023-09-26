fd0d62897f157d918dcf0e1a9854bdd9e7ca5506
18kazee <18kazee2415@gmail.com>
2023-09-26 23:26:57 +0900

    Add: 追加ボタンを押したあと、テキストボックス内の文字を初期化

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const innerText = document.getElementById("add-text").value;
+
+  // テキストボックスの値を初期化
+  document.getElementById("add-text").value = "";
+  alert(innerText);
 };
 
 document
